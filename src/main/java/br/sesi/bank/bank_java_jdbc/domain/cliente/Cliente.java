69cd59bd06717a7b5f2b78ad97880e1d88ce7ca5
JuliaGB007 <juliabernades8@gmail.com>
2024-10-11 12:16:00 -0300

    criando classes + correção de erros + incompleto

---
+++ src/main/java/br/sesi/bank/bank_java_jdbc/domain/cliente/Cliente.java
@@ -0,0 +1,28 @@
+package br.sesi.bank.bank_java_jdbc.domain.cliente;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Cliente(DadosCadastroCliente dados){
+
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public String getCpf (){
+        return this.cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+    public boolean equale(Object o) {
+        return true;
+    }
+    public int hashCode () {
+        return 0;
+    }
+}
